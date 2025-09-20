9951b47fe0c3e069afa76e55192a2e49e566254a
mlukenich <73347955+mlukenich@users.noreply.github.com>
2025-09-20 17:35:42 -0400

    initial

---
+++ src/main/java/com/marketer/affiliate_agent/AffiliateAgentApplication.java
@@ -0,0 +1,13 @@
+package com.marketer.affiliate_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffiliateAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffiliateAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketer/affiliate_agent/AffiliateAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketer.affiliate_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffiliateAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
